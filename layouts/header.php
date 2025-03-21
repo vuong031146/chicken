501f1e17c8912096ae3843c7e2ad4d3ce9748705
Ngo Dinh Nam <ngodinhnam.dev@gmail.com>
2025-03-21 20:06:31 +0700

    first commit

--- layouts/header.php
+++ layouts/header.php
@@ -13,7 +13,6 @@
     <link rel="stylesheet" href="/assest/css/custom.css?=<?= time(); ?>">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11.11.1/dist/sweetalert2.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.11.1/dist/sweetalert2.min.js"></script>
 <style>
     
